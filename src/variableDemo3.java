d18b6e30dbc9cba959349b0866d5d767ff3cbe40
wxl <3355137186@qq.com>
2023-02-09 14:36:57 +0800

    1.2

---
+++ src/variableDemo3.java
@@ -0,0 +1,16 @@
+public class variableDemo3 {
+    public static void main(String[] args) {
+        //byte
+        byte b=10;
+        System.out.println(b);//10
+        //short
+        short s = 20;
+        System.out.println(s);//20
+        //int
+        int i = 30;
+        System.out.println(i);
+        //long
+        long f = 40;
+        System.out.println(f);
+    }
+}
