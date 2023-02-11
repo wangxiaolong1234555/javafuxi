72e23a5e1ad3ecb6d300f6a46e4c84d8958c30c4
wangxiaolong1234555 <3355137186@qq.com>
2023-02-11 13:48:16 +0800

    1.2

---
+++ src/VariableTest2.java
@@ -0,0 +1,22 @@
+public class VariableTest2 {
+    public static void main(String[] args) {
+        //定义变量记录姓名：name
+        String name = "黑马谢广坤";
+        //定义变量记录年龄：age
+        int age = 18;
+        //定义变量记录性别：gender
+        char gender ='男';
+        //定义变量记录身高：height
+        double height = 180.1;
+        //定义变量记录是否单身：flag
+        //true 单身 false 不是单身
+        boolean flag = true;
+
+        //输出变量值
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(height);
+        System.out.println(flag);
+    }
+}
