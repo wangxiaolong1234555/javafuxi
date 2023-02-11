113ddbb9e5c1d2bf68a9ee624ff83dd9f9b8ec32
wangxiaolong1234555 <3355137186@qq.com>
2023-02-11 12:48:16 +0800

    1.2

--- src/variableDemo3.java
+++ src/variableDemo3.java
@@ -10,7 +10,17 @@ public class variableDemo3 {
         int i = 30;
         System.out.println(i);
         //long
-        long f = 40;
+        /*如果要定义long类型的变量，在数值后面必须加L作为后缀，L可以大写也可以小写*/
+        //建议使用大写L
+        long f = 99999999999L;
         System.out.println(f);
+
+        //flog
+        /*注意点：定义float类型的时候，数据值也需要加一个F作为后缀*/
+        float g = 10.1F;
+        System.out.println(g);
+        //double
+        double d =20.2;
+        System.out.println(d);
     }
 }
