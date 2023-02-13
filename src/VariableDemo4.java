d871a58e7443d5275258e3ed09543ab84c65f30a
wangxiaolong1234555 <3355137186@qq.com>
2023-02-13 14:35:10 +0800

    1.2

---
+++ src/VariableDemo4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+//导包 ————Scanner这个类在哪
+public class VariableDemo4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //创建对象————表示我要开始用Scanner这个类了
+        int i = sc.nextInt();
+        //接收数据————真正开始干活了。
+    }
+
+}
