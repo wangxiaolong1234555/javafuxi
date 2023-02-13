60da4e064742772d71dd93d5955191523ab8a323
wangxiaolong1234555 <3355137186@qq.com>
2023-02-13 14:41:09 +0800

    1.2

--- src/VariableDemo4.java
+++ src/VariableDemo4.java
@@ -4,6 +4,7 @@ public class VariableDemo4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //创建对象————表示我要开始用Scanner这个类了
+        System.out.println("请输入i值:");
         int i = sc.nextInt();
         //接收数据————真正开始干活了。
         //变量i记录了键盘录入的数据
